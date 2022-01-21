b56a64d87ed97fd408748caaed43e9e4b0ad99eb
Charlie Morris <charliertm1@gmail.com>
2022-01-21 12:15:02 +0000

    ugly favicon and title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,27 @@
 import { Center } from "@chakra-ui/react";
 import PubInput from "../components/PubInput";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Center h={"100vh"}>
-      <PubInput
-        m={4}
-        p={6}
-        maxW={"800px"}
-        w={"full"}
-        bg={"white"}
-        boxShadow={"2xl"}
-        // rounded={"lg"}
-        pos={"relative"}
-        zIndex={1}
-      />
-    </Center>
+    <>
+      <Head>
+        <title>Wetherspenny</title>
+        <meta property="og:title" content="Wetherspenny" key="title" />
+      </Head>
+      <Center h={"100vh"}>
+        <PubInput
+          m={4}
+          p={6}
+          maxW={"800px"}
+          w={"full"}
+          bg={"white"}
+          boxShadow={"2xl"}
+          // rounded={"lg"}
+          pos={"relative"}
+          zIndex={1}
+        />
+      </Center>
+    </>
   );
 }
