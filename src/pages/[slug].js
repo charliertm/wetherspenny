ed0f04f5fdba37f1dabc46d934bcc2c31f20c134
Charlie Morris <charliertm1@gmail.com>
2022-01-23 15:11:12 +0000

    search bar

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -1,28 +1,75 @@
-import { Center, List } from "@chakra-ui/react";
+import {
+  Center,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  List,
+  VStack,
+  Icon,
+  InputLeftAddon,
+} from "@chakra-ui/react";
 import { slugs_to_ids } from "../slugs_to_ids";
 import fetch from "node-fetch";
 import ErrorPage from "next/error";
 import DrinkCard from "../components/DrinkCard";
+import { useState } from "react";
+import { FaBeer } from "react-icons/fa";
 
 export default function Slug(props) {
   if (!props.data) {
     return <ErrorPage statusCode={404} />;
   }
+  const [drinks, setDrinks] = useState(props.data);
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setValue(event.target.value);
+    // use event.target.value for filter rather than value because state is only available next render
+    setDrinks(
+      props.data.filter((drink) => {
+        return drink.name
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase());
+      })
+    );
+  };
   return (
     <Center>
-      <List>
-        {props.data.map((drink, index) => {
-          return (
-            <DrinkCard
-              key={index}
-              name={drink.name}
-              abv={drink.abv}
-              rank={index + 1}
-              portions={drink.portions}
-            ></DrinkCard>
-          );
-        })}
-      </List>
+      <VStack w={"full"}>
+        <Center pt={6} pb={4} pl={4} pr={4} w={"full"}>
+          <Input
+            placeholder={"Search for your favourites"}
+            p={6}
+            maxW={"800px"}
+            w={"full"}
+            bg={"white"}
+            boxShadow={"lg"}
+            rounded={"lg"}
+            borderWidth={2}
+            borderColor={"black"}
+            onChange={handleChange}
+            value={value}
+            focusBorderColor={"wetherspoons.500"}
+            _hover={{
+              borderColor: "wetherspoons.500",
+            }}
+          />
+        </Center>
+        <List w={"full"}>
+          {drinks.map((drink, index) => {
+            return (
+              <DrinkCard
+                key={index}
+                name={drink.name}
+                abv={drink.abv}
+                rank={drink.rank}
+                portions={drink.portions}
+              ></DrinkCard>
+            );
+          })}
+        </List>
+      </VStack>
     </Center>
   );
 }
@@ -32,7 +79,11 @@ export const getServerSideProps = async ({ params, res }) => {
     const { slug } = params;
     const id = slugs_to_ids[slug];
     const result = await fetch(`https://wetherspenny-api.herokuapp.com/${id}`);
-    const data = await result.json();
+    const dataRecieved = await result.json();
+    const data = dataRecieved.map((drink, index) => ({
+      ...drink,
+      rank: index + 1,
+    }));
     return {
       props: { data },
     };
