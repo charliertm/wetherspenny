984c3aa9c22fdce2899ff91b306a5ced1169c010
Charlie Morris <charliertm1@gmail.com>
2022-01-23 20:19:42 +0000

    add loading with spinner for search

--- src/components/PubInput.js
+++ src/components/PubInput.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { CUIAutoComplete } from "chakra-ui-autocomplete";
 import { useRouter } from "next/router";
 import { pubs_info } from "../pubs_info";
-import { Box, Flex, Text, theme } from "@chakra-ui/react";
+import { Box, Center, Flex, Spinner, Text, theme } from "@chakra-ui/react";
 
 const pubs = pubs_info.map((pub) => {
   return {
@@ -15,9 +15,11 @@ export default function PubInput(props) {
   const [pickerItems, setPickerItems] = useState(pubs);
   const [selectedItems, setSelectedItems] = useState([]);
   const router = useRouter();
+  const [loading, SetLoading] = useState(false);
 
   const handleSelectedItemsChange = (selectedItems) => {
     if (selectedItems) {
+      SetLoading(true);
       setSelectedItems(selectedItems);
       if (selectedItems.length > 0) {
         router.push(selectedItems[0].value.slug);
@@ -38,30 +40,36 @@ export default function PubInput(props) {
 
   return (
     <Box {...props}>
-      <CUIAutoComplete
-        label="Choose your spoons"
-        placeholder="Name of your Spoons"
-        disableCreateItem={true}
-        items={pickerItems}
-        selectedItems={selectedItems}
-        itemRenderer={customRender}
-        onSelectedItemsChange={(changes) =>
-          handleSelectedItemsChange(changes.selectedItems)
-        }
-        inputStyleProps={{
-          borderColor: theme.colors.blackAlpha["400"],
-          focusBorderColor: "wetherspoons.500",
-          _hover: {
-            borderColor: "wetherspoons.500",
-          },
-          borderWidth: 1,
-          boxShadow: "md",
-        }}
-        hideToggleButton={true}
-        highlightItemBg={"blue.200"}
-        listStyleProps={{ maxH: "200px", overflowX: "hidden" }}
-        labelStyleProps={{ alignSelf: "center" }}
-      />
+      {loading ? (
+        <Center>
+          <Spinner speed="0.8s" />
+        </Center>
+      ) : (
+        <CUIAutoComplete
+          label="Choose your spoons"
+          placeholder="Name of your Spoons"
+          disableCreateItem={true}
+          items={pickerItems}
+          selectedItems={selectedItems}
+          itemRenderer={customRender}
+          onSelectedItemsChange={(changes) =>
+            handleSelectedItemsChange(changes.selectedItems)
+          }
+          inputStyleProps={{
+            borderColor: theme.colors.blackAlpha["400"],
+            focusBorderColor: "wetherspoons.500",
+            _hover: {
+              borderColor: "wetherspoons.500",
+            },
+            borderWidth: 1,
+            boxShadow: "md",
+          }}
+          hideToggleButton={true}
+          highlightItemBg={"blue.200"}
+          listStyleProps={{ maxH: "200px", overflowX: "hidden" }}
+          labelStyleProps={{ alignSelf: "center" }}
+        />
+      )}
     </Box>
   );
 }
